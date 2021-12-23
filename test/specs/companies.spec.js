b4c47ac443e994056cb809b38a974fb3d7ebf812
IAN-LIVADNYI <livadny.yan@gmail.com>
2021-12-23 17:48:19 -0500

    edited names of selectors and some minor changes

--- test/specs/companies.spec.js
+++ test/specs/companies.spec.js
@@ -11,26 +11,18 @@ describe("1-CompPage", () => {
 
     it ("Verify if it is the 'Companies page' ", async () => {
         await LoginPage.login("tolik.test1@yahoo.com", "Tolik");
-        await new Promise(resolve => setTimeout(resolve, 5000));
         await PublicationsPage.menuButton.click();
-        await new Promise(resolve => setTimeout(resolve, 5000));
-        await MenuPage.menuCompaniesButton.click();
+        await MenuPage.companiesOption.click();
         await new Promise(resolve => setTimeout(resolve, 5000));
     });
 
     it("Load More button downloaded more companies' card", async () =>{
-        // await LoginPage.login("tolik.test1@yahoo.com", "Tolik");
-        // await PublicationsPage.menuButton.click();
-        await CompaniesPage.loadMoreButton.click()
-        // await new Promise(resolve => setTimeout(resolve, 5000));
+        await CompaniesPage.loadMoreBtn.click()
+        await new Promise(resolve => setTimeout(resolve, 5000));
     })
 
     it("Click On 1st Company Tile", async () => {
-        // await LoginPage.login("tolik.test1@yahoo.com", "Tolik");
-        // await PublicationsPage.menuButton.click();
-        // await MenuPage.menuCompaniesButton.click()
         await CompaniesPage.firstCompanyCard.scrollIntoView()
-        await new Promise(resolve => setTimeout(resolve, 5000));
         await CompaniesPage.firstCompanyCard.click()
         // await new Promise(resolve => setTimeout(resolve, 5000));
     });

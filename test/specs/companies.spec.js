e3127887bdcc62d09c8699ed33a56a5a68807efd
IAN-LIVADNYI <livadny.yan@gmail.com>
2021-12-23 21:22:46 -0500

    new test and function  was added

--- test/specs/companies.spec.js
+++ test/specs/companies.spec.js
@@ -3,22 +3,28 @@ const PublicationsPage = require("../pageobjects/Publications.page")
 const MenuPage = require("../pageobjects/Menu.page")
 const CompaniesPage = require('../pageobjects/Companies.page')
 
-describe("1-CompPage", () => {
+describe("CompaniesPage", () => {
 
     before(() => {
         browser.maximizeWindow();
     })
 
-    it ("Verify if it is the 'Companies page' ", async () => {
+    it ("CP-1: Verify if it is the Companies page", async () => {
         await LoginPage.login("tolik.test1@yahoo.com", "Tolik");
         await PublicationsPage.menuButton.click();
         await MenuPage.companiesOption.click();
-        await new Promise(resolve => setTimeout(resolve, 5000));
+        const title = await CompaniesPage.companiesPageTitle.getText();
+        await expect(title).toEqual('companies')
+    })
+
+    it  ("CP-2: Verify is there the Problems word on 1st company card", async () =>{
+        const problemString = await CompaniesPage.problemsFirstCompany.getText();
+        const newString = CompaniesPage.trimString(problemString);
+        await expect(newString).toEqual('Problems:')
     });
 
-    it("Load More button downloaded more companies' card", async () =>{
+    it("CP-3: Load More button downloaded more companies' card", async () =>{
         await CompaniesPage.loadMoreBtn.click()
-        await new Promise(resolve => setTimeout(resolve, 5000));
     })
 
     it("Click On 1st Company Tile", async () => {

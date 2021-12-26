e378dcd02f242d3240281d109cf85d0a78132149
VitaliiArtemenko <vitaliiart1977@gmail.com>
2021-12-25 18:09:35 -0800

    Added folder with credentials

---
+++ test/data/login,data.js
@@ -0,0 +1,4 @@
+module.exports = {
+    userCredentials: { email: "vitaliiart1977+user@gmail.com", password: "xb76Z_]c:"},
+    adminCredentials: {email: "vitaliiart1977+admin@gmail.com", password: "Lg*6ZFm=G"},
+}

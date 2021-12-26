7d2d8f4e246c3e8900e5123df705c7e775da1233
IAN-LIVADNYI <livadny.yan@gmail.com>
2021-12-25 19:49:25 -0500

    fixed some bugs in the companies.spec.js

--- test/specs/companies.spec.js
+++ test/specs/companies.spec.js
@@ -1,7 +1,7 @@
 const LoginPage = require("../pageobjects/Login.page");
-const PublicationsPage = require("../pageobjects/Publications.page")
-const MenuPage = require("../pageobjects/Menu.page")
-const CompaniesPage = require('../pageobjects/Companies.page')
+const PublicationsPage = require("../pageobjects/Publications.page");
+const MenuPage = require("../pageobjects/Menu.page");
+const CompaniesPage = require('../pageobjects/Companies.page');
 
 describe("CompaniesPage", () => {
 
@@ -11,26 +11,25 @@ describe("CompaniesPage", () => {
 
     it ("CP-1: Verify if it is the Companies page", async () => {
         await LoginPage.login("tolik.test1@yahoo.com", "Tolik");
-        await MenuPage.clickMenu()
+        await MenuPage.clickMenu();
         await MenuPage.companiesOption.click();
         const title = await CompaniesPage.companiesPageTitle.getText();
-        await expect(title).toEqual('companies')
+        await expect(title).toEqual('companies');
     })
 
     it  ("CP-2: Verify is there the Problems word on 1st company card", async () =>{
         const problemString = await CompaniesPage.problemsFirstCompany.getText();
         const newString = CompaniesPage.trimString(problemString);
-        await expect(newString).toEqual('Problems:')
+        await expect(newString).toEqual('Problems:');
     });
 
     it("CP-3: Load More button downloaded more companies' card", async () =>{
-        await CompaniesPage.loadMoreBtn.click()
+        await CompaniesPage.loadMoreBtn.click();
     })
 
     it("Click On 1st Company Tile", async () => {
-        await CompaniesPage.firstCompanyCard.scrollIntoView()
-        await CompaniesPage.firstCompanyCard.click()
-        // await new Promise(resolve => setTimeout(resolve, 5000));
+        await CompaniesPage.firstCompanyCard.scrollIntoView();
+        await CompaniesPage.firstCompanyCard.click();
     });
 
 

d7165a599cd3f746a242c01628106022af62286b
IAN-LIVADNYI <livadny.yan@gmail.com>
2021-12-26 23:55:38 -0500

    some tests were added in people.spec

--- test/specs/people.spec.js
+++ test/specs/people.spec.js
@@ -3,7 +3,9 @@ const LoginData = require('../data/login.data');
 const MenuPage = require("../pageobjects/Menu.page");
 const PeoplePage = require("../pageobjects/People.page")
 const PublicationsPage = require("../pageobjects/Publications.page");
-const CompaniesPage = require('../pageobjects/Companies.page');
+const CompaniesPage = require("../pageobjects/Companies.page");
+const ProfilePage = require("../pageobjects/Profile.page");
+const {userCredentials} = require("../data/login.data");
 
 
 describe("TC1 - Login page", () => {
@@ -15,28 +17,32 @@ describe("TC1 - Login page", () => {
     it("PP-1: Verify if it is the People page", async () => {
         await LoginPage.login(LoginData.userCredentials.email, LoginData.userCredentials.password);
         await MenuPage.clickMenu();
-        await MenuPage.clickPeople()
-        const titleOfPeoplePage = await PeoplePage.usersTitle.getText();
-        await expect(titleOfPeoplePage).toEqual("users");
+        await MenuPage.clickPeople();
+
+        await expect(await PeoplePage.usersTitle.getText()).toEqual("users");
     });
 
     it("PP-2: Verify that MenuPage navigation works from the PeoplePage: to Publication", async () => {
         await PeoplePage.menuButton.click();
         await MenuPage.publicationsOption.click();
-        const titleOfPublicationPage = await PublicationsPage.publicationsTitle.getText();
-        await expect(titleOfPublicationPage).toEqual("publications");
+
+        await expect(await PublicationsPage.publicationsTitle.getText()).toEqual("publications");
         await MenuPage.clickPeople();
-        const titleOfPeoplePage = await PeoplePage.usersTitle.getText();
-        await expect(titleOfPeoplePage).toEqual("users");
+        await expect(await PeoplePage.usersTitle.getText()).toEqual("users");
     });
 
     it("PP-3: Verify that MenuPage navigation works from the PeoplePage: to Companies", async () => {
         await MenuPage.companiesOption.click();
-        const titleOfCompaniesPage = await CompaniesPage.companiesPageTitle.getText();
-        await expect(titleOfCompaniesPage).toEqual("companies");
+        await expect(await CompaniesPage.companiesPageTitle.getText()).toEqual("companies");
         await MenuPage.clickPeople();
-        const titleOfPeoplePage = await PeoplePage.usersTitle.getText();
-        await expect(titleOfPeoplePage).toEqual("users");
+        await expect(await PeoplePage.usersTitle.getText()).toEqual("users");
+    });
+
+    it("PP-5: Verify that MenuPage navigation works from the PeoplePage: to Profile", async () => {
+        await MenuPage.profileOption.click();
+        await expect(await ProfilePage.title.getText()).toEqual("user");
+        await MenuPage.clickPeople();
+        await expect(await PeoplePage.usersTitle.getText()).toEqual("users");
     });
 
 });

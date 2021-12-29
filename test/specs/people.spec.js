f9ab5aa4a44ffcc58cad1cbed388a34d014673f5
IAN-LIVADNYI <livadny.yan@gmail.com>
2021-12-29 14:58:10 -0500

    some tests were added. All tests were refactored

--- test/specs/people.spec.js
+++ test/specs/people.spec.js
@@ -29,49 +29,49 @@ describe("TC1 - People page", () => {
         await expect(await PublicationsPage.publicationsTitle.getText()).toEqual("publications");
     });
 
-    it("Verify that user can get back from the Publication page to the People page", async () => {
+    it("PP-3: Verify that user can get back from the Publication page to the People page", async () => {
         await PeoplePage.getToPeoplePage();
         await expect(await PeoplePage.usersTitle.getText()).toEqual("users");
     });
 
-    it("PP-3: Verify that MenuButton navigation works from the PeoplePage: to Companies", async () => {
+    it("PP-4: Verify that MenuButton navigation works from the PeoplePage: to Companies", async () => {
         await globalNavigationPage.clickMenu();
         await globalNavigationPage.clickCompanies();
 
         await expect(await CompaniesPage.companiesPageTitle.getText()).toEqual("companies");
     });
 
-    it("Verify that user can get back from the Companies page to the People page", async () => {
+    it("PP-5: Verify that user can get back from the Companies page to the People page", async () => {
         await PeoplePage.getToPeoplePage();
         await expect(await PeoplePage.usersTitle.getText()).toEqual("users");
     });
 
-    it("PP-4: Verify that MenuButton navigation works from the PeoplePage: to Problems", async () => {
+    it("PP-6: Verify that MenuButton navigation works from the PeoplePage: to Problems", async () => {
         await globalNavigationPage.clickMenu();
         await globalNavigationPage.clickProblems();
 
         await expect(await ProblemsPage.problemsPageTitle.getText()).toEqual("problems");
         });
 
-    it("Verify that user can get back from the Problems page to the People page", async () => {
+    it("PP-7: Verify that user can get back from the Problems page to the People page", async () => {
         await PeoplePage.getToPeoplePage();
         await expect(await PeoplePage.usersTitle.getText()).toEqual("users");
     });
 
-    it("PP-5: Verify that MenuButton navigation works from the PeoplePage: to Profile", async () => {
+    it("PP-8: Verify that MenuButton navigation works from the PeoplePage: to Profile", async () => {
         await globalNavigationPage.clickMenu();
         await globalNavigationPage.clickProfile();
 
         await expect(await ProfilePage.title.getText()).toEqual("user");
     });
 
-    it("Verify that user can get back from the Profile page to the People page", async () => {
+    it("PP-9: Verify that user can get back from the Profile page to the People page", async () => {
         await PeoplePage.getToPeoplePage();
 
         await expect(await PeoplePage.usersTitle.getText()).toEqual("users");
     });
 
-    it("PP-6: Verify that MenuButton navigation works from the PeoplePage: to Logout", async () => {
+    it("PP-10: Verify that MenuButton navigation works from the PeoplePage: to Logout", async () => {
         await globalNavigationPage.clickMenu();
         await globalNavigationPage.clickLogOut();
 
@@ -80,7 +80,7 @@ describe("TC1 - People page", () => {
         await expect(await LoginPage.btnLogin.getText()).toEqual("LOGIN");
     });
 
-    it("Verify that the user again can be logged in and gotten back to the People page", async () => {
+    it("PP-11: Verify that the user again can be logged in and gotten back to the People page", async () => {
         await LoginPage.login(LoginData.userCredentials.email,LoginData.userCredentials.password);
         await PeoplePage.getToPeoplePage();
 

3a3a0a333a68722c97752c36493816ca9baa30fc
Galina Ramaniuk <galina.sergarz@gmail.com>
2021-12-28 20:37:35 -0500

    chenged layout of getting selectors

--- test/pageobjects/OneCompany.page.js
+++ test/pageobjects/OneCompany.page.js
@@ -2,74 +2,44 @@ const Page = require('./Page');
 
 class OneCompanyPage extends Page {
 
-    get companyPageTitle () {
-        return $('//h6[text()="companies"]');
-    }
+    get companyPageTitle () { return $('//h6[text()="companies"]'); }
 
-    get btnBack() {
-        return $('.btn.btn-link');
-    }
+    get btnBack() { return $('.btn.btn-link'); }
 
-    get oneCompanyImage() {
-        return $('//div[@class = "ant-image"]/img');
-    }
+    get oneCompanyImage() { return $('//div[@class = "ant-image"]/img'); }
 
-    get oneCompanyName() {
-        return $('.mr-3.mb-3 h2');
-    }
+    get oneCompanyName() { return $('.mr-3.mb-3 h2'); }
 
-    get oneCompanyDescription() {
-        return $('.mr-3.mb-3 p');
-    }
+    get oneCompanyDescription() { return $('.mr-3.mb-3 p'); }
 
-    get technicalTasksSentence() {
-        return $('//h5[text()="Technical tasks given during the interview:"]');
-    }
+    get technicalTasksSentence() { return $('//h5[text()="Technical tasks given during the interview:"]'); }
 
-    get firstTechnicalTaskLink() {
-        return $('.d-flex:nth-child(1) a:nth-child(1)');
-    }
+    get firstTechnicalTaskLink() { return $('.d-flex:nth-child(1) a:nth-child(1)'); }
 
-    get titleFirstTechnicalTask() {
-        return $('.d-flex:nth-child(1) a:nth-child(1) h6');
-    }
+    get titleFirstTechnicalTask() { return $('.d-flex:nth-child(1) a:nth-child(1) h6'); }
 
-    get positionFirstTechnicalTask() {
-        return $('a:nth-child(1) .text-truncate:nth-child(2)');
-    }
+    get positionFirstTechnicalTask() { return $('a:nth-child(1) .text-truncate:nth-child(2)'); }
 
-    get likeBtn(){
-        return $('#like-btn');
-    }
+    get likeBtn(){ return $('#like-btn'); }
 
-    get numberOflikes(){
-        return $('#like-btn .ml-1');
-    }
+    get numberOflikes(){ return $('#like-btn .ml-1'); }
 
-    get accountImage(){
-        return $('.d-inline-block img');
-    }
+    get accountImage(){ return $('.d-inline-block img'); }
 
-    get commentBtn(){
-        return $('#comment-btn');
-    }
+    get commentBtn(){ return $('#comment-btn'); }
 
-    get inputCommentField(){
-        return $('#comment-input');
-    }
+    get inputCommentField(){ return $('#comment-input'); }
 
-    get sendBtn(){
-        return $('#send-btn');
-    }
+    get sendBtn(){ return $('#send-btn'); }
 
-    async openCompany() {
+    async oneCompanyPage() {
         await this.openOneCompanyPage();
         await this.likeBtn.click();
         await this.commentBtn.click();
         await this.firstTechnicalTaskLink();
     }
 
-    async clickLoadMore() {
+    async moveBackToCompanies() {
         await this.openOneCompanyPage();
         await this.btnBack.click()
     }

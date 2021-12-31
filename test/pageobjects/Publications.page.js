6b934d7c43d014c21497c110eca63e42689fbb4d
Volha Parmanchuk <chispa85@mail.ru>
2021-12-30 21:16:51 -0800

    Added 2 tests to Publication.spec.js

--- test/pageobjects/Publications.page.js
+++ test/pageobjects/Publications.page.js
@@ -16,9 +16,7 @@ const Page = require ("./Page");
 
         get publications (){return $('div.pb-4>div:nth-child(2)>div>a>div')}
 
-
-
-
+        get comments (){return $('div[class=\'bg-light p-2\'] div:nth-child(2)')}
 
         get btnLoadMore(){return $('//div[@class=\'btn-link\']')}
 
@@ -32,6 +30,17 @@ const Page = require ("./Page");
                 await this.findPublication(publication).$("div>a[href*='/publication/']").getText() :
                 await publication.$("div>a[href*='/publication/']").getText();
             return title;
+        }
+
+        async  createComments() {
+            for (let i = 1; i <= 3; i++) {
+                await this.inputComment.click();
+                await this.inputComment.setValue(`Group Vitalii ${i}`);
+                await this.sendComment.click();
+                await new Promise(resolve => setTimeout(resolve, 2000));
+                let temp = await this.comments.getText();
+                await expect(temp).toEqual(`Group Vitalii ${i}`);
+            }
         }
 
     open() {

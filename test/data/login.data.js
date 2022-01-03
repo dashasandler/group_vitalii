5235704d207999854023c49d4a7a130cd0af7121
VitaliiArtemenko <vitaliiart1977@gmail.com>
2022-01-02 23:24:41 -0800

    API tests: - added 4 different tests with comments for each of them; - added 1 more API method that returns all the data of the response; - added fake emails which can be used

--- test/data/login.data.js
+++ test/data/login.data.js
@@ -7,4 +7,5 @@ module.exports = {
     },
     adminCredentials: {email: "vitaliiart1977+admin@gmail.com", password: "Lg*6ZFm=G"},
     fakeCredentialsForRegistration: {email: `abc${Date.now()}@t.com`, password: 'Abcd1234!'},
+    fakeCredentialsUser2: {email: `qa2${Date.now()}@t.com`, password: '!11234Bcda'},
 }
--- test/specs/api/userAuthApi.spec.js
+++ test/specs/api/userAuthApi.spec.js
@@ -1,25 +1,60 @@
-const { getActivationLinkByCreatingUser, registerActivationLink} = require('../../helpers/apiMethods');
+const {
+    getActivationLinkByCreatingUser,
+    registerActivationLink,
+    getActivationDataByCreatingUser
+} = require('../../helpers/apiMethods');
 const Data = require('../../data/login.data');
+const Chai = require('chai');
+const chaiExpect = Chai.expect;
 
-describe('USER AUTHENTICATION', () => {
-
-    before( () => {
-        browser.maximizeWindow();
-    });
+describe('API - user authentication', () => {
 
     let activationLink = null;
+    let activationData = null;
     let resultOfActivation = null;
+    // let resultOfActivationFromData = null;
 
-    it('User registration', async () => {
+    // This is a simple assertion with getting activation link directly
+    it('User registration - variant1', async () => {
         activationLink = await getActivationLinkByCreatingUser(
             Data.fakeCredentialsForRegistration.email, Data.fakeCredentialsForRegistration.password);
 
-        console.log('************************************************************************');
-        console.log(activationLink);
-        console.log('************************************************************************');
-
         expect(!!activationLink.activationLinkId).toBe(true);
-    })
+    });
 
-    it('')
-})
+    it('User activation - variant1', async () => {
+        resultOfActivation = await registerActivationLink(activationLink.activationLinkId);
+
+        expect(resultOfActivation.activationString).toEqual('Activation Successful!');
+    });
+
+    // This is an assertion with getting data of the response of user activation
+    // If we are getting data we can use a lot more assertions
+    it('User registration - variant2', async () => {
+        activationData = await getActivationDataByCreatingUser(
+            Data.fakeCredentialsUser2.email, Data.fakeCredentialsUser2.password);
+
+        // Here are two identical expects for the status of response: from WebdriverIo and chai
+        expect(activationData.status).toEqual(200);
+        chaiExpect(activationData.status).to.eq(200);
+
+        // Here are two identical expects for the statusText of response: from WebdriverIo and chai
+        expect(activationData.statusText).toEqual('OK');
+        chaiExpect(activationData.statusText).to.be.a('string').that.is.not.empty;
+        chaiExpect(activationData.statusText).to.eq('OK');
+
+        // Below different expects from WebdriverIo and chai
+        expect(!!activationData.data).toBe(true);
+        chaiExpect(activationData.data).to.be.a('object');
+        expect(activationData.data.data.userCreate).not.toBe('empty');
+        chaiExpect(activationData.data.errors).to.be.undefined;
+        chaiExpect(activationData.data.data.userCreate).to.be.a('string').that.is.not.empty;
+        chaiExpect(activationData.data.data.userCreate).not.empty;
+    });
+
+    it('User activation - variant2', async () => {
+       resultOfActivation = await registerActivationLink(activationData.data.data.userCreate);
+
+       expect(resultOfActivation.activationString).toEqual('Activation Successful!');
+    });
+});

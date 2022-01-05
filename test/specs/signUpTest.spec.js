88efcc9fe28317e2dea4302e81962c534b6b7430
iryna.kalynychenko <irzhik1975@gmail.com>
2022-01-05 14:55:49 -0500

    line 13 deleted

--- test/specs/signUpTest.spec.js
+++ test/specs/signUpTest.spec.js
@@ -10,7 +10,6 @@ describe('Login functionality', () => {
     it("Shouldn`t be able to sign up with existing email", async () => {
         await SignUpPage.fillSignUpCredentials(LoginData.userCredentials.email, LoginData.userCredentials.password);
         await SignUpPage.btnSignUp.click();
-        await browser.pause(2000);
         const alertMsg = `User with ${LoginData.userCredentials.email} already exist`
         await expect(SignUpPage.alert).toHaveText(alertMsg);
     });
